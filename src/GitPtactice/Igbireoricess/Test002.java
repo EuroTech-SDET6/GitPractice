49f5830fdd0b76d9f95308d94e9bd0dc7f54191c
Keti19 <keti.toseva4@gmail-com>
2023-02-23 15:37:38 +0100

    Test001,Test002 and Test003 update, TestMave added

--- src/GitPtactice/Igbireoricess/Test002.java
+++ src/GitPtactice/Igbireoricess/Test002.java
@@ -2,7 +2,7 @@ package GitPtactice.Igbireoricess;
 
 public class Test002 {
     public static void main(String[] args) {
-        System.out.println("New");
+        System.out.println("Test 002");
 
         System.out.println("done");
     }
--- src/GitPtactice/day3/Test003.java
+++ src/GitPtactice/day3/Test003.java
@@ -2,6 +2,6 @@ package GitPtactice.day3;
 
 public class Test003 {
     public static void main(String[] args) {
-
+        System.out.println("Test 003");
     }
 }
